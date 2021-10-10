398b35592c3341da1c9b6eb211a5795d23cea44f
christot20 <thomaschristo1234@yahoo.com>
2021-10-10 12:53:47 -0400

    Added computer function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,3 +2,11 @@
 //First to 5 wins
 // If the cpu and players choice are the same, tie and no points given
 //conditionals for rock vs paper vs scissors needed
+console.log("Hello");
+//computer function
+const choices = ["Rock", "Paper", "Scissors"];
+function computerPlay(){
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(randomChoice);
+}
+computerPlay();
