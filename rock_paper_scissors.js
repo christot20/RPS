a2dd9964addd36f8d72b70073ba0b20e70a68f45
christot20 <thomaschristo1234@yahoo.com>
2021-10-10 13:38:17 -0400

    first working version

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,12 +1,29 @@
-//PseudoCode here
-//First to 5 wins
-// If the cpu and players choice are the same, tie and no points given
-//conditionals for rock vs paper vs scissors needed
+//test
 console.log("Hello");
-//computer function
-const choices = ["Rock", "Paper", "Scissors"];
-function computerPlay(){
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(randomChoice);
+const choices = ["Rock", "Paper", "Scissors"]; //array of choices
+function computerPlay(){ //computer choice function
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)]; //chooses random value from array
+    console.log(randomChoice); //print random value
+    return randomChoice;
 }
 computerPlay();
+function playRound(){
+    compChoice = randomChoice;
+    playerChoice = prompt("What will you choose?");
+    if (playerChoice === "Rock" && compChoice === "Scissors"){
+        console.log("You Win!");
+    }
+    else if (playerChoice === "Paper" && compChoice === "Rock"){
+        console.log("You Win!");
+    }
+    else if (playerChoice === "Scissors" && compChoice === "Paper"){
+        console.log("You Win!");
+    }
+    else if (playerChoice === compChoice){
+        console.log("Tie");
+    }
+    else{ 
+        console.log("You Lose");
+    }
+}
+playRound();
