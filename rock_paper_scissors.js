b0c73ad4083e943aa6de3fcf9e4cb03252579379
christot20 <thomaschristo1234@yahoo.com>
2021-10-10 23:15:55 -0400

    Added Conditions for winning

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,29 +1,31 @@
 //test
 console.log("Hello");
-const choices = ["Rock", "Paper", "Scissors"]; //array of choices
 function computerPlay(){ //computer choice function
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)]; //chooses random value from array
-    console.log(randomChoice); //print random value
-    return randomChoice;
+    const choices = ["Rock", "Paper", "Scissors"]; //array of choices
+    return choices[Math.floor(Math.random() * choices.length)]; //chooses random value from array
 }
-computerPlay();
-function playRound(){
-    compChoice = randomChoice;
-    playerChoice = prompt("What will you choose?");
+function playRound(compChoice, playerChoice){
+    console.log(playerChoice);
+    console.log(compChoice);
     if (playerChoice === "Rock" && compChoice === "Scissors"){
         console.log("You Win!");
+        return "You Win!";
     }
     else if (playerChoice === "Paper" && compChoice === "Rock"){
         console.log("You Win!");
+        return "You Win!";
     }
     else if (playerChoice === "Scissors" && compChoice === "Paper"){
         console.log("You Win!");
+        return "You Win!";
     }
     else if (playerChoice === compChoice){
         console.log("Tie");
+        return "Tie";
     }
     else{ 
-        console.log("You Lose");
+        console.log("You Lose.");
+        return "You Lose";
     }
 }
-playRound();
+playRound(computerPlay(), prompt("What will you choose?")); //parameters of compChoice and 
