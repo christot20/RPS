5b65fe479e31d674e33fa98fbe3c5739eb8b869e
christot20 <thomaschristo1234@yahoo.com>
2021-10-11 22:35:11 -0400

    Added loop to round function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,31 +1,64 @@
 //test
+i = 0;
+j = 0;
 console.log("Hello");
 function computerPlay(){ //computer choice function
     const choices = ["Rock", "Paper", "Scissors"]; //array of choices
     return choices[Math.floor(Math.random() * choices.length)]; //chooses random value from array
 }
+
 function playRound(compChoice, playerChoice){
+    function game(){
+        while (i <= 5 || j <= 5) {
+            if (i === 5){
+                console.log("You Win");
+                return "You Win!";
+            }
+            else if (j === 5){
+                console.log("You lose");
+                return "You Lose";
+            }
+            else{
+                return playRound(computerPlay(), prompt("What will you choose?"));
+            }
+        } 
+    } 
     console.log(playerChoice);
     console.log(compChoice);
     if (playerChoice === "Rock" && compChoice === "Scissors"){
         console.log("You Win!");
+        i++;
+        console.log(i, j);
+        game();
         return "You Win!";
     }
     else if (playerChoice === "Paper" && compChoice === "Rock"){
         console.log("You Win!");
+        i++;
+        console.log(i, j);
+        game();
         return "You Win!";
     }
     else if (playerChoice === "Scissors" && compChoice === "Paper"){
         console.log("You Win!");
+        i++;
+        console.log(i, j);
+        game();
         return "You Win!";
     }
     else if (playerChoice === compChoice){
         console.log("Tie");
+        console.log(i, j);
+        game();
         return "Tie";
     }
     else{ 
         console.log("You Lose.");
+        j++;
+        console.log(i, j);
+        game();
         return "You Lose";
     }
+    
 }
-playRound(computerPlay(), prompt("What will you choose?")); //parameters of compChoice and 
+playRound(computerPlay(), prompt("What will you choose?"));
