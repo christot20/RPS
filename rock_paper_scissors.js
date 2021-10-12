d02862cfbc52a35ce65367f8539ca505b81fff42
christot20 <thomaschristo1234@yahoo.com>
2021-10-12 12:56:18 -0400

    Added comments to script

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,63 +1,63 @@
 //test
-i = 0;
-j = 0;
+i = 0; //player score variable
+j = 0; //computer score variable
 console.log("Hello");
 function computerPlay(){ //computer choice function
     const choices = ["Rock", "Paper", "Scissors"]; //array of choices
     return choices[Math.floor(Math.random() * choices.length)]; //chooses random value from array
 }
 
-function playRound(compChoice, playerChoice){
-    function game(){
+function playRound(compChoice, playerChoice){ //round function
+    function game(){  //round loop function, first to 5 wins, loop ends
         while (i <= 5 || j <= 5) {
-            if (i === 5){
+            if (i === 5){   //if human wins
                 console.log("You Win");
-                return "You Win!";
+                return "You Won!";
             }
-            else if (j === 5){
+            else if (j === 5){  //if computer wins
                 console.log("You lose");
-                return "You Lose";
+                return "You Lost!";
             }
             else{
-                return playRound(computerPlay(), prompt("What will you choose?"));
+                return playRound(computerPlay(), prompt("What will you choose? Rock, paper, or scissors?"));
             }
         } 
     } 
     console.log(playerChoice);
     console.log(compChoice);
-    if (playerChoice === "Rock" && compChoice === "Scissors"){
+    if (playerChoice === "Rock" && compChoice === "Scissors"){  //if player wins conditionals
         console.log("You Win!");
         i++;
         console.log(i, j);
         game();
-        return "You Win!";
+        return "You won the round!";
     }
     else if (playerChoice === "Paper" && compChoice === "Rock"){
         console.log("You Win!");
         i++;
         console.log(i, j);
         game();
-        return "You Win!";
+        return "You won the round!";
     }
     else if (playerChoice === "Scissors" && compChoice === "Paper"){
         console.log("You Win!");
         i++;
         console.log(i, j);
         game();
-        return "You Win!";
+        return "You won the round!";
     }
-    else if (playerChoice === compChoice){
+    else if (playerChoice === compChoice){   //if the round is a tie
         console.log("Tie");
         console.log(i, j);
         game();
-        return "Tie";
+        return "The round is a tie!";
     }
     else{ 
-        console.log("You Lose.");
+        console.log("You Lose.");   //if computer wins the round
         j++;
         console.log(i, j);
         game();
-        return "You Lose";
+        return "You lost the round!";
     }
     
 }
