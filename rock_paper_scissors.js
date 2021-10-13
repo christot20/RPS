cce3e0b9c18aa0aa9341de8332810870e24896e3
christot20 <thomaschristo1234@yahoo.com>
2021-10-12 23:07:29 -0400

    added case insensitivty and allowed for player to retry if answer isn't conditional

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -3,43 +3,42 @@ i = 0; //player score variable
 j = 0; //computer score variable
 console.log("Hello");
 function computerPlay(){ //computer choice function
-    const choices = ["Rock", "Paper", "Scissors"]; //array of choices
+    const choices = ["rock", "paper", "scissors"]; //array of choices
     return choices[Math.floor(Math.random() * choices.length)]; //chooses random value from array
 }
-
-function playRound(compChoice, playerChoice){ //round function
-    function game(){  //round loop function, first to 5 wins, loop ends
-        while (i <= 5 || j <= 5) {
-            if (i === 5){   //if human wins
-                console.log("You Win");
-                return "You Won!";
-            }
-            else if (j === 5){  //if computer wins
-                console.log("You lose");
-                return "You Lost!";
-            }
-            else{
-                return playRound(computerPlay(), prompt("What will you choose? Rock, paper, or scissors?"));
-            }
-        } 
+function game(){  //round loop function, first to 5 wins, loop ends
+    while (i <= 5 || j <= 5) {
+        if (i === 5){   //if human wins
+            console.log("You Win");
+            return "You Won!";
+        }
+        else if (j === 5){  //if computer wins
+            console.log("You lose");
+            return "You Lost!";
+        }
+        else{
+            return playRound(computerPlay(), prompt("What will you choose? Rock, Paper, or Scissors?").toLowerCase());
+        }
     } 
+} 
+function playRound(compChoice, playerChoice){ //round function
     console.log(playerChoice);
     console.log(compChoice);
-    if (playerChoice === "Rock" && compChoice === "Scissors"){  //if player wins conditionals
+    if (playerChoice === "rock" && compChoice === "scissors"){  //if player wins conditionals
         console.log("You Win!");
         i++;
         console.log(i, j);
         game();
         return "You won the round!";
     }
-    else if (playerChoice === "Paper" && compChoice === "Rock"){
+    else if (playerChoice === "paper" && compChoice === "rock"){
         console.log("You Win!");
         i++;
         console.log(i, j);
         game();
         return "You won the round!";
     }
-    else if (playerChoice === "Scissors" && compChoice === "Paper"){
+    else if (playerChoice === "scissors" && compChoice === "paper"){
         console.log("You Win!");
         i++;
         console.log(i, j);
@@ -52,6 +51,11 @@ function playRound(compChoice, playerChoice){ //round function
         game();
         return "The round is a tie!";
     }
+    else if(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
+        console.log("invalid answer");
+        game();
+        return "Please enter either Rock, Paper, or scissors.";
+    }
     else{ 
         console.log("You Lose.");   //if computer wins the round
         j++;
@@ -61,4 +65,4 @@ function playRound(compChoice, playerChoice){ //round function
     }
     
 }
-playRound(computerPlay(), prompt("What will you choose?"));
+playRound(computerPlay(), prompt("What will you choose?").toLowerCase());
